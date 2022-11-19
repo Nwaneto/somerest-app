5fe5944fe85cf1bb0bb2975048041509f096deaa
the-infinite <toby.johnson.precious@gmail.com>
2022-11-19 11:41:06 +0100

    Added: The support home message to the mobile version.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -716,9 +716,8 @@ class _HomePageState extends State<HomePage> {
 				alignment: Alignment.topLeft,
 				margin: const EdgeInsets.only(top: 30, bottom: 30),
 				width: screenSize.width * 0.95,
-				padding: const EdgeInsets.only(
-					top: 10, bottom: 10, left: 50, right: 50),
-					decoration: BoxDecoration(
+				padding: ResponsiveWidget.isSmallScreen(context) ? const EdgeInsets.only(top: 10, bottom: 10, left:  20, right: 20) : const EdgeInsets.only(top: 10, bottom: 10, left:  50, right: 50),
+				decoration: BoxDecoration(
 					border: Border.all(
 						color: Colors.blueGrey,
 						width: 2,
@@ -729,7 +728,9 @@ class _HomePageState extends State<HomePage> {
 					mainAxisAlignment: MainAxisAlignment.start,
 					crossAxisAlignment: CrossAxisAlignment.start,
 					children: ResponsiveWidget.isLargeScreen(context)
-						? [
+						? 
+						
+						[
 							const Text("Leave a message",
 								textAlign: TextAlign.start,
 								style: TextStyle(
@@ -878,7 +879,145 @@ class _HomePageState extends State<HomePage> {
 						]
 
 						// For smaller screens
-						: [],
+						: [
+							const Text("Leave a message",
+								textAlign: TextAlign.start,
+								style: TextStyle(
+									color: Colors.black,
+									fontSize: 18,
+									fontWeight: FontWeight.w700
+								)
+							),
+							
+							Container (
+								margin: const EdgeInsets.only(top: 30),
+								width: screenSize.width * 0.9,
+								child: TextField(
+									cursorColor: Colors.blue,
+									controller: _nameController,
+									decoration: const InputDecoration(
+										labelText: "Name",
+										hintText: "Required",
+										border: OutlineInputBorder(
+											borderRadius: BorderRadius.all(
+												Radius.circular(10)
+											),
+
+											borderSide: BorderSide(
+												color: Colors.blue,
+												width: 1.0,
+											)
+										)
+									),
+								)
+							),
+
+							Container(
+								margin: const EdgeInsets.only(top: 15),
+								width: screenSize.width * 1,
+								child: TextField(
+									cursorColor: Colors.blue,
+									controller: _emailController,
+									decoration: const InputDecoration(
+										labelText: "Email",
+										hintText: "Required",
+										border: OutlineInputBorder(
+											borderRadius: BorderRadius.all(
+												Radius.circular(10)
+											),
+
+											borderSide: BorderSide(
+												color: Colors.blue,
+												width: 1.0,
+											)
+										)
+									),
+								)
+							),
+
+							Container(
+								margin: const EdgeInsets.only(top: 15),
+								width: screenSize.width * 0.9,
+								child: TextField(
+									cursorColor: Colors.blue,
+									controller: _subjectController,
+									decoration: const InputDecoration(
+										labelText: "Subject",
+										hintText: "Required",
+										border: OutlineInputBorder(
+											borderRadius: BorderRadius.all(
+												Radius.circular(10)
+											),
+
+											borderSide: BorderSide(
+												color: Colors.blue,
+												width: 1.0,
+											)
+										)
+									),
+								)
+							),
+
+							// The Message Body
+							Container(
+								margin: const EdgeInsets.only(top: 15),
+								height: screenSize.height * 0.25,
+								width: screenSize.width * 0.9,
+								child: TextField(
+									maxLines: 12,
+									cursorColor: Colors.blue,
+									controller: _messageController,
+									decoration: const InputDecoration(
+										labelText: "Your Message",
+										hintText: "Required",
+										border: OutlineInputBorder(
+											borderRadius: BorderRadius.all(
+												Radius.circular(10)
+											),
+
+											borderSide: BorderSide(
+												color: Colors.blue,
+												width: 1.0,
+											)
+										)
+									),
+								)
+							),
+
+							Container(
+								padding: const EdgeInsets.all(10),
+								margin: const EdgeInsets.only(top: 30),
+								decoration: const BoxDecoration(
+									color: Colors.blue,
+									borderRadius: BorderRadius.all(Radius.circular(10))
+								),
+								child: TextButton(
+									onPressed: () {
+										var name = _nameController.text;
+										var email = _emailController.text;
+										var subject = _subjectController.text;
+										var message = _messageController.text;
+
+										// After everything, reorder the state of the widget
+										setState(() {
+											_nameController.text = "";
+											_emailController.text = "";
+											_subjectController.text = "";
+											_messageController.text = "";
+										});
+									},
+							
+									child: const Text(
+										"Submit",
+										textAlign: TextAlign.start,
+										style: TextStyle(
+											fontSize: 18,
+											color: Colors.white
+										)
+									)
+								),
+							)
+						],
 				)),
 
 			// Next is the footer of the website.
